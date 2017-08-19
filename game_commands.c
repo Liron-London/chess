9e4600f46ca55e4630da1266cf01ec5ca8f824b8
Galsa <galsdh@gmail.com>
2017-08-19 12:57:10 +0300

    game.c and game.h were changed for compilations purpose

--- game_commands.c
+++ game_commands.c
@@ -14,14 +14,14 @@ Gamecommand game_command_parse_line(const char* str){
 	location* target;
 	move* cur_move;
 
-	Gamecommand* game_command;
+	Gamecommand game_command;
 
 	// char* command_int = strtok(NULL, " \t\n");
 
 
 	if (strcmp(command_text, "move") == true){
 		// need to be checked
-		game_command->cmd = MOVE;
+		game_command.cmd = MOVE;
 
 		source->row = atoi(strtok(str_copy, "<,>"));
 		source->column = strtok(str_copy, "<,>")[0];
@@ -29,41 +29,41 @@ Gamecommand game_command_parse_line(const char* str){
 		target->column = strtok(str_copy, "<,>")[0];
 
 		//TODO -- need to verify that locations are intialized well
-		game_command->move->source = source;
-		game_command->move->dest = target;
-		game_command->validArg = true;
+		game_command.move->source = source;
+		game_command.move->dest = target;
+		game_command.validArg = true;
 
 		return game_command;
 	}
 
 	if (strcmp(command_text, "quit") == true){
-		game_command->cmd = GAME_QUIT;
-		game_command->validArg = true;
+		game_command.cmd = GAME_QUIT;
+		game_command.validArg = true;
 		return game_command;
 	}
 
 	if (strcmp(command_text, "undo") == true){
-		game_command->cmd = UNDO;
-		game_command->validArg = true;
+		game_command.cmd = UNDO;
+		game_command.validArg = true;
 		return game_command;
 	}
 
 	if (strcmp(command_text, "reset") == true){
-		game_command->cmd = RESET;
-		game_command->validArg = true;
+		game_command.cmd = RESET;
+		game_command.validArg = true;
 		return game_command;
 	}
 
 	if (strcmp(command_text, "reset") == true){
-		game_command->cmd = RESET;
-		game_command->validArg = true;
+		game_command.cmd = RESET;
+		game_command.validArg = true;
 		return game_command;
 	}
 
 	if (strcmp(command_text, "save") == true){
 		//need to complete saving mode
-		game_command->cmd = SAVE;
-		game_command->validArg = true;
+		game_command.cmd = SAVE;
+		game_command.validArg = true;
 		return game_command;
 	}
 
