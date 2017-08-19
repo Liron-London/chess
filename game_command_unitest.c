825145fc178bb337a2c47a16620951d97b7292f6
Galsa <galsdh@gmail.com>
2017-08-19 12:31:45 +0300

    game.c and game.h were changed for compilations purpose

---
+++ game_command_unitest.c
@@ -0,0 +1,29 @@
+/*
+ * game_command_unitest.c
+ *
+ *  Created on: Aug 19, 2017
+ *      Author: Gal
+ */
+
+#include "game_command.h"
+#include "game.h"
+#include <stdbool.h>
+
+
+static bool basic_check(){
+	game* game = game_create();
+	Gamecommand game_command;
+
+	game_command = game_command_parse_line("move <2, A> to <3, B>");
+	printf("%d", game_command->move->source->row);
+	printf("%c", game_command->move->source->column);
+	printf("%d", game_command->move->dest->row);
+	printf("%c", game_command->move->dest->column);
+
+int main() {
+	bool success;
+	success = basic_check();
+	if (success)
+		printf("basic settings checks success!\n");
+	return 0;
+}
