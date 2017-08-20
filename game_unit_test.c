c935d06dbead7a009e739b3624b9e0726f71a301
Galsa <galsdh@gmail.com>
2017-08-20 14:20:08 +0300

    game.c and game.h were changed for compilations purpose

--- game_unit_test.c
+++ game_unit_test.c
@@ -59,10 +59,11 @@ static bool game_print_test() {
 static bool move_pawns_check(){
 	game* game = game_create();
 	print_board(game);
-	Gamecommand* game_command;
+	Gamecommand* game_command = malloc(sizeof(game_command));
 	game_command = game_command_parse_line("move <2,B> to <2,C>");
 	move_piece(game, game_command.move, &(game->whites[8]));
 	print_board(game);
+	free(game_command);
 	return true;
 }
 
