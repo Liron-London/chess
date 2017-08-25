8da248cd744bfa71b01fe061e84a00ce1be46647
Galsa <galsdh@gmail.com>
2017-08-25 12:35:15 +0300

    DEBUG

--- game.c
+++ game.c
@@ -52,7 +52,7 @@ piece* create_piece(){
 	}
 	piece->alive = 1;
 	piece->color = 0;
-	piece->piece_location = (location*)(malloc(sizeof(location)));
+	piece->piece_location = (malloc(sizeof(location)));
 
 	// default values
 	piece->piece_location->row = -1;
