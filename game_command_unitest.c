3c49a6a89b15e5c7f15b3d1dff79442a60598322
Galsa <galsdh@gmail.com>
2017-09-06 22:22:26 +0300

    debug

--- game_command_unitest.c
+++ game_command_unitest.c
@@ -39,10 +39,12 @@ static bool comp_move_check(){
 	print_board(cur_game);
 	move* cur_move = create_move();
 	cur_move = get_recommended_move_for_comp(cur_game, 3);
+	printf("MOVE IS: ROW: %d, COL: %d", cur_move->source->row, cur_move->source->column);
+	printf("MOVE IS: ROW: %d, COL: %d", cur_move->dest->row, cur_move->dest->column);
 	piece* my_piece = location_to_piece(cur_game, cur_move->source);
 	move_piece(cur_game, cur_move, my_piece);
 	print_board(cur_game);
-	destroy_move(cur_move);
+	//destroy_move(cur_move);
 	return true;
 }
 
