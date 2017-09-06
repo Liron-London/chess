0f103c832205d615803c30981cf838db97451998
Galsa <galsdh@gmail.com>
2017-09-06 22:15:09 +0300

    debug

--- moves.c
+++ moves.c
@@ -459,6 +459,7 @@ void queen_valid_moves(location** valid_locs, game* cur_game, piece* cur_piece) 
 		if (valid_locs[i]->row == -1 && valid_locs[i]->column == -1) {
 			new_ptr_to_valid_locs = &valid_locs[i];
 			DEBUG2("index is %d\n", i);
+			DEBUG2("valid_locs is ROW: %d COL: %d\n", valid_locs[i]->row, valid_locs[i]->column);
 		}
 	}
 	rook_valid_moves(new_ptr_to_valid_locs, cur_game, cur_piece);
