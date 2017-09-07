7540604362d950ff58ed9b6528c57cd9cd700d5d
Galsa <galsdh@gmail.com>
2017-09-07 23:43:35 +0300

    debug

--- minimax.c
+++ minimax.c
@@ -74,7 +74,7 @@ int scoring_function(game* game) {
 // returns the move the computer should do, the move will be in best_move
 int alphabeta(game* node, int depth, int alpha, int beta, bool maximizing_player, move* best_move){
 	if (depth == 0){
-		DEBUG2("Score is %d\n", scoring_function(node));
+		//DEBUG2("Score is %d\n", scoring_function(node));
 		return scoring_function(node);
 	}
 
