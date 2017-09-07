e5db92449b9161f64bcdfe3088f799b5fd1d1976
Galsa <galsdh@gmail.com>
2017-09-07 20:00:42 +0300

    debug

--- minimax.c
+++ minimax.c
@@ -86,7 +86,7 @@ int alphabeta(game* node, int depth, int alpha, int beta, bool maximizing_player
 	move* tmp_move = create_move();
 	int color = (node->current_turn + node->user_color + 1)%2;
 	location** valid_locs = malloc(64*sizeof(location*));
-	move* tmp_best_move = create_move()
+	move* tmp_best_move = create_move();
 
 
 	for (int i=0; i<64; i++){
