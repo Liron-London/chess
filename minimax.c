2ba67a915b82afd4a9b5b4f7651fce74be50109a
Galsa <galsdh@gmail.com>
2017-09-08 00:19:08 +0300

    debug

--- minimax.c
+++ minimax.c
@@ -96,7 +96,6 @@ int alphabeta(game* node, int depth, int alpha, int beta, bool maximizing_player
 		valid_locs[i] = create_location();
 	}
 
-	// only the minimum part
 	if (maximizing_player == true){
 		DEBUG2("******************\n");
 		tmp_score = -10000000;
@@ -136,7 +135,7 @@ int alphabeta(game* node, int depth, int alpha, int beta, bool maximizing_player
 					j += 1;
 
 					move_piece(tmp_game, tmp_move, location_to_piece(tmp_game, tmp_move->source));
-					DEBUG2("current move is: ROW:%d COL: %d to ROW: %d COL: %d\n", tmp_best_move->source->row, tmp_best_move->source->column, tmp_best_move->dest->row, tmp_best_move->dest->column);
+					DEBUG2("PC current move is: ROW:%d COL: %d to ROW: %d COL: %d\n", tmp_best_move->source->row, tmp_best_move->source->column, tmp_best_move->dest->row, tmp_best_move->dest->column);
 
 					new_score = alphabeta(tmp_game, depth-1, alpha, beta, false, best_move);
 					//new_score = max(tmp_score, alphabeta(tmp_game, depth-1, alpha, beta, false, best_move));
@@ -219,6 +218,8 @@ int alphabeta(game* node, int depth, int alpha, int beta, bool maximizing_player
 
 					j += 1;
 					move_piece(tmp_game, tmp_move, location_to_piece(tmp_game, tmp_move->source));
+
+					DEBUG2("User current move is: ROW:%d COL: %d to ROW: %d COL: %d\n", tmp_best_move->source->row, tmp_best_move->source->column, tmp_best_move->dest->row, tmp_best_move->dest->column);
 
 					new_score = min(tmp_score, alphabeta(tmp_game, depth-1, alpha, beta, false, best_move));
 
