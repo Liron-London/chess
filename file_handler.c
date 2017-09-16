98c43132cce04f8f3e723441c00230a87d01c39d
Galsa <galsdh@gmail.com>
2017-09-16 11:16:33 +0300

    load pieces fixed

--- file_handler.c
+++ file_handler.c
@@ -29,6 +29,7 @@ void update_pieces_for_load(game* cur_game){
 			if (cur_game->board[i][j] != EMPTY_ENTRY){
 				if (cur_game->board[i][j] > 'a'){
 					printf("ROW: %d, COL: %d\n", i, j);
+					printf("white index is: %d/n", white_index);
 					// check if white king
 					if (cur_game->whites[white_index]->piece_type != 'k'){
 						cur_game->whites[white_index]->piece_type = cur_game->board[i][j];
