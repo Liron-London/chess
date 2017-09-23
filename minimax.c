620f8ef8445f664f26c9b0f890d4086aa6ba4f09
Galsa <galsdh@gmail.com>
2017-09-23 18:37:12 +0300

    debug

--- minimax.c
+++ minimax.c
@@ -62,16 +62,13 @@ int scoring_function(game* game) {
 			black_sum += 100;
 		}
 	}
-	// if it's the white player's turn:
-
-
 
 	if (current_turn_color(game) == 1) {
 		score = white_sum - black_sum;
 	} else {
 		score = black_sum - white_sum;
 	}
-	return -score;
+	return score;
 }
 
 
@@ -263,12 +260,12 @@ move* get_recommended_move_for_comp(game* game, int depth){
 	move* comp_move = create_move();
 	// user is black
 	if (game->user_color == 0){
-		alphabeta(game, depth, INT_MIN, INT_MAX, true, comp_move);
+		alphabeta(game, depth, INT_MIN, INT_MAX, false, comp_move);
 	}
 
 	// user is white
 	if (game->user_color == 1){
-		alphabeta(game, depth, INT_MIN, INT_MAX, false, comp_move);
+		alphabeta(game, depth, INT_MIN, INT_MAX, true, comp_move);
 	}
 	return comp_move;
 }
