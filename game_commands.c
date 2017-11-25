ad002b9389286bf1c784a893c15931a8330de97e
Galsa <galsdh@gmail.com>
2017-11-25 10:52:27 +0200

    testing game_commands for undo

--- game_commands.c
+++ game_commands.c
@@ -368,13 +368,13 @@ int game_play(game* game){
 					first_tmp_move = array_list_get_last_move(history);
 					announce_undo_move(current_turn_color(game), first_tmp_move);
 					array_list_remove_last(history);
-					destroy_move(first_tmp_move);
+					//destroy_move(first_tmp_move);
 					
 					move* second_tmp_move = create_move();
 					second_tmp_move = array_list_get_last_move(history);
 					announce_undo_move((current_turn_color(game)+1)%2, second_tmp_move);
 					array_list_remove_last(history);
-					destroy_move(second_tmp_move);
+					//destroy_move(second_tmp_move);
 					printf("C\n");
 					
 					printf("A\n");
