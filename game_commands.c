40dba0e35fc7d7cba18b304d5ff4be2d6cdbe196
Galsa <galsdh@gmail.com>
2017-11-25 11:13:23 +0200

    testing game_commands for undo

--- game_commands.c
+++ game_commands.c
@@ -382,7 +382,7 @@ int game_play(game* game){
 					
 					printf("C\n");
 					
-					printf("tmp move is %d, %d\n", tmp_move->dest->row, tmp_move->loc->row);
+					printf("tmp move is %d, %d\n", tmp_move->dest->row, tmp_move->source->row);
 					destroy_move(tmp_move);
 					
 					printf("D\n");
